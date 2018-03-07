83f576c5ff026a72c3eb9656db7665f20f2e09e2
Meetasha29 <meetaasha29@gmail.com>
2018-03-07 16:45:12 +0530

    styling logi page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,10 +5,13 @@ import {connect} from 'react-redux';
 import {startLogin} from '../actions/auth';
 
 const LoginPage = ({startLogin}) => (
-  <div>
-  <button
-    onClick={startLogin}>login</button>
-</div>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expensify</h1>
+      <p className="box-layout__subtitle">It's time to get your expenses under control.</p>
+      <button class="button" onClick={startLogin}>Login with Google</button>
+    </div>
+  </div>
 );
 
 const mapDispatchToProps = (dispatch) =>({
